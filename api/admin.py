97ad439d43070414fc59bb47ec7d0be618423a3c
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-08-08 10:58:11 +0800

    feature: register models in admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Intern, Company, Project, Supervisor
 
 # Register your models here.
+admin.site.register(Intern)
+admin.site.register(Company)
+admin.site.register(Project)
+admin.site.register(Supervisor)
